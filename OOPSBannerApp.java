772c1255b528a60f3421dd03d35c8b8cc5e08878
harry5822 <pratham.8vibrant@gmail.com>
2026-03-10 22:22:47 +0530

    UC3 - Refactor banner rendering using String.join()

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,7 +1,25 @@
-package com.bridgelabz.banner;
+/**
+ * OOPSBannerApp
+ * UC3 – Render OOPS banner using String.join()
+ */
 
-public class UC1 {
+public class OOPSBannerApp {
+
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println(String.join(" ",
+                " *** ", " *** ", " **** ", " **** "));
+        System.out.println(String.join(" ",
+                "*   *", "*   *", "*   *", "*    "));
+        System.out.println(String.join(" ",
+                "*   *", "*   *", "*   *", "*    "));
+        System.out.println(String.join(" ",
+                "*   *", "*   *", "**** ", " *** "));
+        System.out.println(String.join(" ",
+                "*   *", "*   *", "*    ", "    *"));
+        System.out.println(String.join(" ",
+                "*   *", "*   *", "*    ", "    *"));
+        System.out.println(String.join(" ",
+                " *** ", " *** ", "*    ", "**** "));
     }
 }
